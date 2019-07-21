5faf131a56f802ee510464add2fc69e9ecdcb124
Kyler Barber <kylerbarber820@yahoo.com>
2019-07-21 05:31:52 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+array.include?(element)
 end
 
 def using_sort(array)
-
+villas = ["wow", "thor", "loki", "I"]
+villas.sort
 end
 
 def using_reverse(array)
-
+flavors = ["wow", "chocolate", "vanilla", "arrays!"]
+flavors.reverse
 end
 
 def using_first(array)
-
+stars = ["wow", "medium", "mild", "blazing"]
+stars.first
 end
 
 def using_last(array)
-
+planets = ["mars", "earth", "pluto", "arrays!"]
+planets.last
 end
 
 def using_size(array)
-
+copy = ["c1", "c2", "c3", "c4", "c5", "c6"]
+copy.size
 end
